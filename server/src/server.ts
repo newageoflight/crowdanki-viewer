dddcb0c4c94ade40cb8d2ea70dc86ad23d68ae94
newageoflight <camint3rnal@live.com>
2020-12-17 00:31:26 +1100

    Adds todo comments to server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ const app = express();
 const port = 4000;
 // const httpServer = new http.Server(app);
 // const io = new IoServer(httpServer);
+// move to mongodb to store decks
+// also should consider finding some way to store images in db but in a way compatible with the current setup
 app.use(express.static(path.join("public", "media")))
 
 app.get("/getdata", (req, res) => {
@@ -26,5 +28,8 @@ app.get("/getdata", (req, res) => {
 //         console.log("User disconnected");
 //     })
 // })
+// Only one problem, how do you display this in the app?
+// Getting the frontend editor to emit events is easy enough but you'll run into problems with display
+// Need to make cursors highlighted with names (how to do this???)
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
